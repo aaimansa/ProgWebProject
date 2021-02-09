d17a8a4dea172aee2cccca1e29a517a9d8577697
aaimansa <61957837+aaimansa@users.noreply.github.com>
2021-02-09 15:14:16 +0700

    Add files via upload

---
+++ inventaris.sql
@@ -0,0 +1,101 @@
+-- MySQL dump 10.13  Distrib 5.6.26, for Win32 (x86)
+--
+-- Host: localhost    Database: inventaris
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `inventaris`
+--
+
+DROP TABLE IF EXISTS `inventaris`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventaris` (
+  `NoLaporan` varchar(5) NOT NULL,
+  `TanggalLapor` date NOT NULL,
+  `KodeKamar` varchar(5) NOT NULL,
+  `Kerusakan` varchar(200) NOT NULL,
+  `Status` varchar(20) NOT NULL,
+  PRIMARY KEY (`NoLaporan`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventaris`
+--
+
+LOCK TABLES `inventaris` WRITE;
+/*!40000 ALTER TABLE `inventaris` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventaris` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kamar`
+--
+
+DROP TABLE IF EXISTS `kamar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kamar` (
+  `KodeKamar` varchar(5) NOT NULL,
+  `asrama` varchar(10) NOT NULL,
+  `NoKamar` varchar(5) NOT NULL,
+  PRIMARY KEY (`KodeKamar`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kamar`
+--
+
+LOCK TABLES `kamar` WRITE;
+/*!40000 ALTER TABLE `kamar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kamar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `taruna`
+--
+
+DROP TABLE IF EXISTS `taruna`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `taruna` (
+  `npm` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `KodeKamar` varchar(5) NOT NULL,
+  PRIMARY KEY (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taruna`
+--
+
+LOCK TABLES `taruna` WRITE;
+/*!40000 ALTER TABLE `taruna` DISABLE KEYS */;
+/*!40000 ALTER TABLE `taruna` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-09 15:12:22
